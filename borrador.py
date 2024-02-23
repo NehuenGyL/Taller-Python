548fdf3130f0f1d1c8a4d5400a4e6f1ee436779d
Ariel Walter Romero Vera <awromero@gyl.local>
2024-02-23 15:27:08 -0300

    borrador

--- borrador.py
+++ borrador.py
@@ -36,25 +36,23 @@ class TestEjercicio01(unittest.TestCase):
 
 
 class TestEjercicio02(unittest.TestCase):
-    def test_imprimir_numeros(self):
+    def test_funcion_02(self):
         # Redirigiendo la entrada estándar
         with unittest.mock.patch('builtins.input', return_value='5'):
             with io.StringIO() as mock_stdout:
                 sys.stdout = mock_stdout
-                pb.imprimir_numeros()
+                pb.funcion_02()
                 output = mock_stdout.getvalue()
                 self.assertEqual(output.strip(), "0\n1\n2\n3\n4\n5")
 
 
-
-
 class TestEjercicio03(unittest.TestCase):
-    def test_verificar_par_o_impar(self):
+    def test_funcion_03(self):
         # Caso de prueba 1: Número par
         with unittest.mock.patch('builtins.input', return_value='6'):
             with io.StringIO() as mock_stdout:
                 sys.stdout = mock_stdout
-                pb.verificar_par_o_impar()
+                pb.funcion_03()
                 output = mock_stdout.getvalue().strip()
                 self.assertEqual(output, "El número 6 es par.")
 
@@ -62,66 +60,45 @@ class TestEjercicio03(unittest.TestCase):
         with unittest.mock.patch('builtins.input', return_value='9'):
             with io.StringIO() as mock_stdout:
                 sys.stdout = mock_stdout
-                pb.verificar_par_o_impar()
+                pb.funcion_03()
                 output = mock_stdout.getvalue().strip()
                 self.assertEqual(output, "El número 9 es impar.")
 
 
-
 class TestEjercicio04(unittest.TestCase):
-    def test_saludar(self):
-        nombre_usuario = "Juan"
-        expected_saludo = "Hola, Juan, ¿cómo estás?"
-        
+    def test_funcion_04(self):
+        nombre_usuario = "Pepito"  # Nombre genérico para la prueba
+        expected_saludo = f"Hola, {nombre_usuario}, ¿cómo estás?"  # Modificado para usar el nombre genérico
+
         with io.StringIO() as mock_stdout:
             sys.stdout = mock_stdout
-            pb.saludar(nombre_usuario)
+            mensaje_saludo = pb.funcion_04(nombre_usuario)
             output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, expected_saludo)
-
+            # Verificar que el nombre del usuario esté en el saludo
+            self.assertIn(nombre_usuario, mensaje_saludo)
 
 
 class TestEjercicio05(unittest.TestCase):
-    def test_tabla_de_multiplicar(self):
+    def test_funcion_05(self):
         # Redirigiendo la entrada estándar
-        with unittest.mock.patch('builtins.input', return_value='3'):
+        with unittest.mock.patch('builtins.input', return_value='5'):
             with io.StringIO() as mock_stdout:
                 sys.stdout = mock_stdout
-                pb.tabla_de_multiplicar()
+                pb.funcion_05()
                 output = mock_stdout.getvalue().strip()
-                expected_output = "Tabla de multiplicar del 3:\n3 x 1 = 3\n3 x 2 = 6\n3 x 3 = 9\n3 x 4 = 12\n3 x 5 = 15"
+                expected_output = "Tabla de multiplicar del 5:\n5 x 1 = 5\n5 x 2 = 10\n5 x 3 = 15\n5 x 4 = 20\n5 x 5 = 25"
                 self.assertEqual(output, expected_output)
 
 
 class TestEjercicio06(unittest.TestCase):
-    def test_clasificar_edades(self):
-        # Caso de prueba 1: Edad menor a 5 años
-        with io.StringIO() as mock_stdout:
-            sys.stdout = mock_stdout
-            pb.clasificar_edades(3)
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, "Eres un bebé")
-
-        # Caso de prueba 2: Edad entre 5 y 12 años
-        with io.StringIO() as mock_stdout:
-            sys.stdout = mock_stdout
-            pb.clasificar_edades(10)
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, "Eres un niño")
-
-        # Caso de prueba 3: Edad entre 13 y 19 años
-        with io.StringIO() as mock_stdout:
-            sys.stdout = mock_stdout
-            pb.clasificar_edades(16)
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, "Eres un adolescente")
-
-        # Caso de prueba 4: Edad igual o mayor a 20 años
-        with io.StringIO() as mock_stdout:
-            sys.stdout = mock_stdout
-            pb.clasificar_edades(25)
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, "Eres un adulto")
+    def test_funcion_06(self):
+        # Redirigiendo la entrada estándar
+        with unittest.mock.patch('builtins.input', return_value='15'):
+            with io.StringIO() as mock_stdout:
+                sys.stdout = mock_stdout
+                pb.funcion_06()
+                output = mock_stdout.getvalue().strip()
+                self.assertEqual(output, "Eres un adolescente")
 
 
 if __name__ == '__main__':
@@ -132,13 +109,10 @@ if __name__ == '__main__':
     hc_errores = len(resultado_test.result.errors) + hc_fallas  # Sumar las fallas como errores
     hc_ok = hc_tests - hc_errores
 
-    archivo_test = open('resultado_test.csv', 'w')
-    archivo_test.write('Total_Tests,Errores,Correctos\n')
-    archivo_test.write(str(hc_tests) + ',' + str(hc_errores) + ',' + str(hc_ok) + '\n')
-    archivo_test.close()
+    with open('resultado_test.csv', 'w') as archivo_test:
+        archivo_test.write('Total_Tests,Errores,Correctos\n')
+        archivo_test.write(f"{hc_tests},{hc_errores},{hc_ok}\n")
 
     print('Resumen')
-    print('Total Correctos:', str(hc_ok))
-    print('Total Errores:', str(hc_errores))
-
-
+    print('Total Correctos:', hc_ok)
+    print('Total Errores:', hc_errores)
